d758bb6db198169c72bda107876787f2266aa676
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-11-25 16:45:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,10 @@
 # out the current state.
 board = ["   x    |   x   |   x   ", "-----------", "      |       |      ", "-----------", ["   o    |   o   |   o   "]
 def display_board(board)
- print "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}"
+ print "#{board[0]}"
+ print "#{board[1]}}"
+ print "#{board[2]}"
+ print "#{board[3]}"
+ print "#{board[4]}"
+ print "#{board[5]}"
 end
